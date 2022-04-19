6ca6df0668525aba4e0a45db8c9c0bd087856b06
michaelcoitim <michaelcoitim1@gmail.com>
2022-04-19 18:18:38 -0300

    criando jogador

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ canvas.width=1024;
 canvas.height=514;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+//criando jogador e inimigo 
+class Sprite {
+    constructor(position){
+        this.position =position
+
+    }
+
+}
+
+const jogador = new Sprite()
