fa6a5a757cf8c177702608df0815697a9616d0d1
michaelcoitim <michaelcoitim1@gmail.com>
2022-04-19 21:35:04 -0300

    Ajuste chão(areia movediça)

--- index.js
+++ index.js
@@ -25,11 +25,13 @@ class Sprite {
     //atualiza desenho 
     update(){
         this.draw()
-        this.velocity.y += gravity
+        
         this.position.y += this.velocity.y
 
         if(this.position.y + this.height +this.velocity.y >= canvas.height){
             this.velocity.y = 0
+        }else{
+            this.velocity.y += gravity
         }
     }
 
