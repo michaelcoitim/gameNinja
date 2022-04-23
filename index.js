c8887d17f1cb13d5a07cd55f8281f1349f587dca
michaelcoitim <michaelcoitim1@gmail.com>
2022-04-22 23:57:47 -0300

    desenhando os ataques

--- index.js
+++ index.js
@@ -16,12 +16,26 @@ class Sprite {
         this.velocity =velocity
         this.height =150
         this.lastKey
+        this.attackBox = {
+            position: this.position ,
+            width: 100 ,
+            height: 50
+
+        }
 
     }
     // desenha
     draw(){
         c.fillStyle='red'
         c.fillRect(this.position.x, this.position.y, 50, this.height)
+
+        //attack Box
+        c.fillRect(
+            this.attackBox.position.x,
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height
+            )
     }
     //atualiza desenho 
     update(){
