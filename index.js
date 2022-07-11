ded9faa25334d96bf1b98ac482ed23c085fa40df
michaelcoitim <michaelcoitim1@gmail.com>
2022-07-10 21:30:45 -0300

    fundo atualizando

--- index.js
+++ index.js
@@ -52,6 +52,56 @@ class Sprite {
     //atualiza desenho 
     update(){
         this.draw()
+
+        
+    }
+
+
+}
+
+
+class Fighter {
+    constructor({position, velocity , color = 'red', offSet}){
+        this.position =position
+        this.velocity =velocity
+        this.width = 50
+        this.height =150
+        this.lastKey
+        this.attackBox = { //ataque
+            position: {
+                x: this.position.x, 
+                y: this.position.y
+            } ,
+            offSet, //delocamento do ataque
+            width: 100 ,
+            height: 50
+
+        }
+        this.color= color
+        this.isAttacking
+        this.health =100
+
+    }
+    // desenha
+    draw(){
+        c.fillStyle= this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //attack Box
+        if(this.isAttacking){
+        c.fillStyle='green'
+        c.fillRect(
+            this.attackBox.position.x,
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height
+            )
+        }
+
+    }
+    //atualiza desenho 
+    update(){
+        this.draw()
         this.attackBox.position.x = this.position.x + this.attackBox.offSet.x
         this.attackBox.position.y = this.position.y
 
@@ -74,6 +124,8 @@ class Sprite {
     }
 
 }
+
+
 
 const jogador = new Sprite({
     position: {
