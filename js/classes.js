480d38b43b863779a54ddc57a7ba76e235c50010
michaelcoitim <michaelcoitim1@gmail.com>
2022-07-10 22:00:05 -0300

    separando as classes

---
+++ js/classes.js
@@ -0,0 +1,86 @@
+class Sprite {
+    constructor({position}){
+        this.position =position
+        this.width = 50
+        this.height =150
+
+    }
+    // desenha
+    draw(){ }
+
+    //atualiza desenho 
+    update(){
+        this.draw()
+
+        
+    }
+
+
+}
+
+
+class Fighter {
+    constructor({position, velocity , color = 'red', offSet}){
+        this.position =position
+        this.velocity =velocity
+        this.width = 50
+        this.height =150
+        this.lastKey
+        this.attackBox = { //ataque
+            position: {
+                x: this.position.x, 
+                y: this.position.y
+            } ,
+            offSet, //delocamento do ataque
+            width: 100 ,
+            height: 50
+
+        }
+        this.color= color
+        this.isAttacking
+        this.health =100
+
+    }
+    // desenha
+    draw(){
+        c.fillStyle= this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        //attack Box
+        if(this.isAttacking){
+        c.fillStyle='green'
+        c.fillRect(
+            this.attackBox.position.x,
+            this.attackBox.position.y,
+            this.attackBox.width,
+            this.attackBox.height
+            )
+        }
+
+    }
+    //atualiza desenho 
+    update(){
+        this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offSet.x
+        this.attackBox.position.y = this.position.y
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if(this.position.y + this.height +this.velocity.y >= canvas.height){
+            this.velocity.y = 0
+        }else{
+            this.velocity.y += gravity
+        }
+        
+    }
+
+    attack(){
+        this.isAttacking = true
+        setTimeout( () => {
+            this.isAttacking =false
+        }, 100)
+    }
+
+}
+
